f00efb7db577b1d0a9c289dfa4802a1f45cb45a2
pkjGIt <pradyumna.hyd@gmail.com>
2020-04-19 11:10:47 +0530

    T11 modified at Br1 branch & line 3 added too

--- my1st.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var dt = require('./2');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("current date & time from Litu's PC" + dt.myDateTime()+ "\n\n");
-  res.write(req.url); 	
-  res.end('Hi This is pradyumna!');
-}).listen(8080)
